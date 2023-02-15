0eb15223c8318c1d9954eb2d043fa3a37d7379b0
sjuiju <jonhny@gmail.com>
2023-02-15 08:40:10 +0700

    upload thu muc gddrm vo github

---
+++ nobita/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from. models import nobita
+admin.site.register(nobita)
---
+++ nobita/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+class nobita(models.Model):
+       tennv = models.CharField(max_length=800)
+       nghenghiep = models.CharField(max_length=1200)
+       khanang = models.CharField(max_length=1500)
